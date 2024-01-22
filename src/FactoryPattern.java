c0f15bbf0df1be0944a70f8021ce769547fae4f6
anto <antokumar00@gmail.com>
2024-01-22 16:15:55 +0600

    error solve

--- src/FactoryPattern.java
+++ src/FactoryPattern.java
@@ -1,7 +1,7 @@
 public class FactoryPattern {
     public static void main(String[] args)
     {
-        Shape shape1 = ShapeFactory.getShape("squre");shape1.draw();
+        Shape shape1 = ShapeFactory.getShape("square");shape1.draw();
         Shape shape2 = ShapeFactory.getShape("rectangle");shape2.draw();
 
 
--- src/ShapeFactory.java
+++ src/ShapeFactory.java
@@ -1,21 +1,31 @@
 public class ShapeFactory {
-    public static Shape ins = null;
+    public static ShapeFactory ins = null;
     private ShapeFactory(){}
 
     public static Shape getShape(String shapeTye)
     {
-        if(ins != null) return ins;
 
-        if(shapeTye == null) return null;
+        if(shapeTye == null)
+            return null;
 
-        if(shapeTye.equalsIgnoreCase("rectangel"))
+        if(shapeTye.equalsIgnoreCase("rectangle"))
         {
             return  new Rectangle();
         }
-        else if(shapeTye.equalsIgnoreCase("rectangel"))
+        else if(shapeTye.equalsIgnoreCase("square"))
         {
             return new Square();
         }
          return  null;
+    }
+
+    private static ShapeFactory getInstance()
+    {
+        if(ins == null)
+        {
+            ins = new ShapeFactory();
+
+        }
+        return  ins;
     }
 }
