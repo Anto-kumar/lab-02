bebc1c21c8fc7808ec350d2aed68ddfd667f7f86
anto <antokumar00@gmail.com>
2024-01-22 15:51:58 +0600

    Shape factory added

--- src/FactoryPattern.java
+++ src/FactoryPattern.java
@@ -1,3 +1,9 @@
 public class FactoryPattern {
-    public static void main
+    public static void main(String[] args)
+    {
+        Shape shape1 = ShapeFactory.getShape("squre")shape1.draw();
+        Shape shape2 = ShapeFactory.getShape("rectangle")shape2.draw();
+
+
+    }
 }
--- src/ShapeFactory.java
+++ src/ShapeFactory.java
@@ -1,2 +1,21 @@
 public class ShapeFactory {
+    public static Shape ins = null;
+    private ShapeFactory(){}
+
+    public static Shape getShape(String shapeTye)
+    {
+        if(ins != null) return ins;
+
+        if(shapeTye == null) return null;
+
+        if(shapeTye.equalsIgnoreCase("rectangel"))
+        {
+            return  new Rectangle();
+        }
+        else if(shapeTye.equalsIgnoreCase("rectangel"))
+        {
+            return new Square();
+        }
+         return  null;
+    }
 }
